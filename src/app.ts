fb66f13541b94ea95605cc4b1976d001c5afc74b
joaobb <joao.pedro.barbosa@ccc.ufcg.edu.br>
2023-05-11 22:46:40 -0300

    Add cors handler

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,13 @@ import express from "express";
 import { routes } from "./routes";
 import { AppDataSource } from "./data-source";
 import { errorMiddleware } from "./middleware/error";
+import cors from "cors";
 
 AppDataSource.initialize().then(() => {
   const PORT = process.env.APP_PORT;
   const app = express();
 
+  app.use(cors());
   app.use(express.json());
   app.use(routes);
 
